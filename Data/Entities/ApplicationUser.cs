5128584e79a2ac690a2b1c7f271921bdcd092610
galinamiteva <galina.miteva@utb.ecutbildning.se>
2024-06-12 17:48:08 +0200

    bytta database

--- Data/Entities/ApplicationUser.cs
+++ Data/Entities/ApplicationUser.cs
@@ -12,7 +12,7 @@ public class ApplicationUser : IdentityUser
     public DateTime? Created { get; set; }
     public DateTime? Modified { get; set; }
 
-    public string? UserAddressId { get; set; }
+    public int? UserAddressId { get; set; }
     public virtual UserAddress? UserAddress { get; set; }
     public string? PreferredEmail { get; set; }
     public bool SubscribeNewsletter { get; set; } = false;
